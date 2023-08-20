e3f5c5348ea9cdd69ac0cdbbf20d861605d5f74c
phentality <pheonix09@yandex.ru>
2023-08-20 16:18:02 +0500

    First iter project 13

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
 const { PORT = 3000 } = process.env;
 
 const app = express();
+app.use(express.static('public'));
 
 app.use((req, res, next) => {
   req.user = {
