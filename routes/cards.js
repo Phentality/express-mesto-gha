9a217df492b34bb0882ed4d0eb6d15a82084eef2
phentality <pheonix09@yandex.ru>
2023-08-20 15:07:34 +0500

    First iter project 13

--- routes/cards.js
+++ routes/cards.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 const {
-  getCards, createCard, deleteCard,
+  getCards, createCard, deleteCard, setLike, deleteLike,
 } = require('../controllers/cards');
 
 router.get('/', getCards);
 router.delete('/:id', deleteCard);
 router.post('/', createCard);
+router.put('/:id/likes', setLike);
+router.delete('/:id/likes', deleteLike);
 
 module.exports = router;
