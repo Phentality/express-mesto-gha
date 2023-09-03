6786e515f0be5470b2a46d9194e0c8bb9605e8aa
phentality <pheonix09@yandex.ru>
2023-09-03 19:45:55 +0500

    First

--- models/user.js
+++ models/user.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const isEmail = require('validator/lib/isEmail');
-const isURL = require('validator/lib/isURL');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -18,9 +17,7 @@ const userSchema = new mongoose.Schema({
   avatar: {
     type: String,
     // eslint-disable-next-line no-useless-escape
-    validate: {
-      validator: (v) => isURL(v),
-    },
+    pattern: '/[-a-zA-Z0-9@:%_\+.~#?&\/=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&\/=]*)?/gi',
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
   },
   email: {
